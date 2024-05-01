d9325d31824d3806c9e3aeec69ff66b3945a358d
The-bAk-Dev <hamadi.the.music.therapist@gmail.com>
2024-05-01 02:25:13 +0200

    working on the featured section almost done it

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,38 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+// je commçerai par la logique de la fonction que j'appellerai
+// appès attaché siute à un scroll event listenner ^_^
+// la fonction du scroll header tributaire du nbre de pixel scrollé horizontalement -50px-
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+// l'appel de la fonction suite à un scroll event :
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper('.popular__container', {
+  loop: true,
+  spaceBetween: 24,
+  SlidesPerView: 'auto',
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    DynamicsBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
